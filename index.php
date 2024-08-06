408d25a75ff22077c4be455995f2e5dc9f5d9774
gabriellandgraf <gabriellandgrafdecarvalho54@gmail.com>
2024-08-06 17:20:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/*
+echo date ('Y-m-d H:i:s') . "<br/>";
+date_default_timezone_set('America/Sao_Paulo');
+echo date ('d/m/Y H:i');
+
+
+echo date ('d');
+echo date ('D');
+
+date_default_timezone_set('Asia/Tokyo');
+echo date ('d/m/Y H:i');
+
+
+$nomes= ['nomeum', ' nomedois', ' nometres'];
+echo $nomes[1];
+
+
+
+$palavras = array("maçã", "banana", "laranja", "uva", "manga", "abacaxi", "pêssego",
+"kiwi", "morango", "melancia");
+echo $palavras[6];
+
+
+$usuarios= ["nome:" => "Creitu",
+			"email" => "creitindugrau@gmail.com",
+			"idade" => "12"
+];
+
+print_r ($usuarios);
+*/
+
+?>
